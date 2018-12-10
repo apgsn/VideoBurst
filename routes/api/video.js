24644fe597c325d11b528ed0f7278999a5faa8e9
apgsn <apgsn@yahoo.com>
2018-12-10 22:23:06 +0100

    Add backend Video validation

--- routes/api/video.js
+++ routes/api/video.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const youtubeUrl = require("youtube-url");
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const secretOrKey = require("../../config/keys").secretOrKey;
@@ -21,15 +22,15 @@ router.post(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const youtubeURI = "https://www.googleapis.com/youtube/v3/videos";
-
     const errors = {};
-    /*const { errors, isInvalid } = validateRegister(req.body);
 
-  if (isInvalid) {
-    console.log(errors);
-    return res.status(400).json(errors);
-  }*/
-    Video.findOne({ videoId: req.body.videoId })
+    const videoId = youtubeUrl.extractId(req.body.videoUrl);
+    if (!videoId) {
+      errors.videoUrl = "Invalid Url";
+      return res.status(400).json(errors);
+    }
+
+    Video.findOne({ videoId: videoId })
       .then(video => {
         if (video) {
           errors.video = "Sorry, this video has already been posted";
@@ -39,7 +40,7 @@ router.post(
             .get(youtubeURI, {
               params: {
                 key: youtubeApiKey,
-                id: req.body.videoId,
+                id: videoId,
                 part: "snippet"
               }
             })
@@ -47,14 +48,18 @@ router.post(
               video = video.data.items[0];
               const newVideo = new Video({
                 user: req.user.id,
-                videoId: req.body.videoId,
+                videoId,
                 channel: video.snippet.channelId,
                 title: video.snippet.title,
                 thumbnail: video.snippet.thumbnails.medium.url
               });
               newVideo.save().then(video => res.json(video));
             })
-            .catch(err => console.log(err));
+            .catch(err => {
+              console.log(err);
+              errors.youtube = "Sorry, this video doesn't exist";
+              return res.status(400).json(errors);
+            });
         }
       })
       .catch(err => console.log(err));
