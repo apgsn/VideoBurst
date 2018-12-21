1ca81feca61bab85ed6b2c79acb1366e8a65f83c
apgsn <apgsn@yahoo.com>
2018-12-21 22:47:16 +0100

    Refactor Like.js

--- client/src/components/common/Like.js
+++ client/src/components/common/Like.js
@@ -4,28 +4,23 @@ import { likeVideo } from "../../actions/videoActions";
 import "./Like.css";
 
 class Like extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      errors: {}
-    };
-  }
-
   onClick = e => {
     console.log("clicky");
     this.props.likeVideo(this.props.videoElem.videoId);
   };
 
+  isLiked = () => {
+    const userId = this.props.auth.isAuthenticated
+      ? this.props.auth.user.id
+      : null;
+    const liked = this.props.videoElem.likes
+      .map(like => like._id)
+      .filter(like => like === userId).length;
+    return liked ? " liked" : "";
+  };
+
   render() {
-    const auth = this.props.auth.isAuthenticated;
-    const userId = auth ? this.props.auth.user.id : null;
-    const likesList = this.props.videoElem.likes.map(likes => likes._id);
-    console.log(likesList);
-    const color = likesList.filter(like => like === userId).length
-      ? "liked"
-      : "";
-    console.log(color);
-    const classes = "col panel-icon-like " + color;
+    const classes = "col panel-icon-like " + this.isLiked();
     return (
       <div className={classes} onClick={this.onClick}>
         <i className="panel-icons fas fa-heart" />
