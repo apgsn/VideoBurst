e588156f681d2a1900d7e959bde994f736650aa9
apgsn <apgsn@yahoo.com>
2019-01-01 20:14:08 +0100

    Better integration with Redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
 const initialState = {};
@@ -8,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
