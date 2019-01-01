59763577af8c502ed917506540b94b2c4692b295
apgsn <apgsn@yahoo.com>
2019-01-01 17:51:58 +0100

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
@@ -31,4 +32,11 @@ app.use("/api/user", user);
 app.use("/api/video", video);
 
 const port = process.env.port || 5000;
+
+// Deployment middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(port, () => console.log("Server running on port " + port));
