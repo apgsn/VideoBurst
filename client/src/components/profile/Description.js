1b4b6da1988668ace20cde58a351c79ef1ed6b4f
apgsn <apgsn@yahoo.com>
2019-11-25 19:49:59 +0100

    Refactor description, fix url scheme for social links

--- client/src/components/profile/Description.js
+++ client/src/components/profile/Description.js
@@ -4,14 +4,18 @@ import { connect } from "react-redux";
 import { changeDescription } from "../../actions/userActions.js";
 import "./Description.css";
 
+import DescriptionTitle from "./DescriptionTitle.js";
+import DescriptionForm from "./DescriptionForm.js";
 import Input from "../common/Input.js";
 
 class Description extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editingBio: false,
-      editingSocial: false,
+      editing: {
+        bio: false,
+        social: false,
+      },
       bio: "",
       social: {
         youtube: "",
@@ -23,6 +27,30 @@ class Description extends Component {
     };
   }
 
+  static socialMap = [
+    {
+      name: 'youtube',
+      description: 'Youtube profile',
+    },
+    {
+      name: 'twitter',
+      description: 'Twitter profile',
+    },
+    {
+      name: 'facebook',
+      description: 'Facebook profile',
+    },
+    {
+      name: 'instagram',
+      description: 'Instagram profile',
+    },
+    {
+      name: 'website',
+      description: 'a personal website',
+      icon: 'globe',
+    },
+  ];
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.profile.bio !== this.state.bio) {
       this.setState({ bio: nextProps.profile.bio });
@@ -32,244 +60,129 @@ class Description extends Component {
     }
   }
 
-  onBioChange = e => this.setState({ bio: e.target.value });
+  onBioChange = e => this.setState({
+    bio: e.target.value,
+  });
 
-  onChange = e => {
-    this.setState({
-      social: {
-        ...this.state.social,
-        [e.target.name]: e.target.value,
-      }
+  onSocialChange = e => this.setState({
+    social: {
+      ...this.state.social,
+      [e.target.name]: e.target.value,
+    }
+  });
+
+  toggleForm = field => this.setState({
+    editing: {
+      ...this.state.editing,
+      [field]: !this.state.editing[field],
+    }
+  });
+
+  onSubmit = field => e => {
+    e.preventDefault();
+    this.toggleForm(field);
+    this.props.changeDescription({
+      data: this.state[field],
+      type: field,
     });
   };
 
-  onIconClick = e => {
-    e.preventDefault();
-    this.setState({ [e.target.id]: !this.state[e.target.id] });
-  };
+  getIconClasses = (site) => {
+    const { icon, name } = site;
+    const fa = icon ? 'fas' : 'fab';
+    return `fa-2x ${fa} fa-${icon || name}`;
+  }
 
-  onBioSubmit = e => {
-    e.preventDefault();
-    this.setState({ editingBio: false });
-    const { bio } = this.state;
-    this.props.changeDescription({ data: bio, type: "bio" });
-  };
 
-  onSocialSubmit = e => {
-    e.preventDefault();
-    this.setState({ editingSocial: false });
-    const { social } = this.state;
-    this.props.changeDescription({ data: social, type: "social" });
-  };
+  /** Rendering functions */
 
-  render() {
-    const { profile } = this.props;
-    const { personal } = this.props;
+  renderSocialInputFields = () => Description.socialMap
+    .map(el => (
+      <div className="d-inline-flex p-2 w-100" key={el.name}>
+        <Input
+          placeholder={`Link to ${el.description}`}
+          value={this.state.social[el.name]}
+          name={el.name}
+          onChange={this.onSocialChange}
+        />
+        <div className="icon-div my-1">
+          <i className={this.getIconClasses(el)} />
+        </div>
+      </div>
+    ));
+
+  renderSocialIcons = (social) => Description.socialMap
+    .filter(el => social[el.name])
+    .map(el => (
+      <a
+        key={el.name}
+        className=" p-2"
+        href={`https://${social[el.name].replace(/^https?:\/\//, '')}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className={this.getIconClasses(el)} />
+      </a>
+    ))
+
+  render = () => {
+    const { profile = {}, personal } = this.props;
+    const { social = {}, username } = profile;
+    const { editing, bio } = this.state;
 
     return (
       <React.Fragment>
-        <h4 className="my-3">
-          {/* bio */}
-          About this user{" "}
-          {personal ? (
-            this.state.editingBio ? (
-              <i
-                id="editingBio"
-                className="fas fa-times text-primary edit mx-2"
-                style={{ cursor: "pointer" }}
-                onClick={this.onIconClick}
-              />
-            ) : (
-                <i
-                  id="editingBio"
-                  className="fas fa-edit text-primary edit mx-2"
-                  style={{ cursor: "pointer" }}
-                  onClick={this.onIconClick}
-                />
-              )
-          ) : null}
-        </h4>
-        {this.state.editingBio ? (
-          <div className="row desc-form-container input bg-light p-3 mb-2 border rounded shadow border-grey">
-            <form
-              className="form-group w-100"
-              autoComplete="off"
-              onSubmit={this.onBioSubmit}
-            >
-              <div className="form-group ">
-                <textarea
-                  placeholder="Write something about you..."
-                  className="form-control w-100"
-                  name="bio"
-                  onChange={this.onBioChange}
-                  value={this.state.bio}
-                />
-              </div>
-              <button
-                className="btn btn-primary m-1 w-25 btn-block mt-4"
-                type="submit"
-              >
-                <i className="fas fa-arrow-circle-right" />
-              </button>
-            </form>
-          </div>
-        ) : (
-            <p className="bio">
-              {this.state.bio ? (
-                <span>{this.state.bio}</span>
-              ) : (
-                  <span className="font-italic">
-                    {profile.username} doesn't have a bio
-              </span>
-                )}
-            </p>
-          )}
+        {/* bio */}
+        <DescriptionTitle
+          name="bio"
+          title="About this user"
+          isEditing={editing.bio}
+          canEdit={personal}
+          onClick={this.toggleForm}
+        />
+        {editing.bio ?
+          <DescriptionForm onSubmit={this.onSubmit("bio")}>
+            <textarea
+              placeholder="Write something about you..."
+              className="form-control w-100"
+              name="bio"
+              onChange={this.onBioChange}
+              value={bio}
+            />
+          </DescriptionForm>
+          :
+          bio ?
+            <span>
+              {bio}
+            </span>
+            :
+            <span className="font-italic">
+              {username} doesn't have a bio
+            </span>
+        }
 
         {/* social */}
-        <h4 className="my-3">
-          Social{" "}
-          {personal ? (
-            this.state.editingSocial ? (
-              <i
-                id="editingSocial"
-                className="fas fa-times text-primary edit mx-2"
-                style={{ cursor: "pointer" }}
-                onClick={this.onIconClick}
-              />
-            ) : (
-                <i
-                  id="editingSocial"
-                  className="fas fa-edit text-primary edit mx-2"
-                  style={{ cursor: "pointer" }}
-                  onClick={this.onIconClick}
-                />
-              )
-          ) : null}
-        </h4>
-        {this.state.editingSocial ? (
-          <div className="row desc-form-container input bg-light p-3 mb-2 border rounded shadow border-grey">
-            <form autoComplete="off" onSubmit={this.onSocialSubmit}>
-              <div className="d-inline-flex p-2 w-100">
-                <Input
-                  placeholder="Link to Youtube profile"
-                  value={this.state.social.youtube}
-                  name="youtube"
-                  onChange={this.onChange}
-                />
-                <div className="icon-div my-1">
-                  <i className="fab fa-youtube fa-2x" />
-                </div>
-              </div>
-              <div className="d-inline-flex p-2">
-                <Input
-                  placeholder="Link to Twitter profile"
-                  value={this.state.social.twitter}
-                  name="twitter"
-                  onChange={this.onChange}
-                />
-                <div className="icon-div my-1">
-                  <i className="fab fa-twitter fa-2x" />
-                </div>
-              </div>
-              <div className="d-inline-flex p-2">
-                <Input
-                  placeholder="Link to Facebook profile"
-                  value={this.state.social.facebook}
-                  name="facebook"
-                  onChange={this.onChange}
-                />
-                <div className="icon-div my-1">
-                  <i className="fab fa-facebook fa-2x" />
-                </div>
-              </div>
-              <div className="d-inline-flex p-2">
-                <Input
-                  placeholder="Link to Instagram profile"
-                  value={this.state.social.instagram}
-                  name="instagram"
-                  onChange={this.onChange}
-                />
-                <div className="icon-div my-1">
-                  <i className="fab fa-instagram fa-2x" />
-                </div>
-              </div>
-              <div className="d-inline-flex p-2">
-                <Input
-                  placeholder="Link to a personal website"
-                  value={this.state.social.website}
-                  name="website"
-                  onChange={this.onChange}
-                />
-                <div className="icon-div my-1">
-                  <i className="fas fa-globe fa-2x" />
-                </div>
-              </div>
-              <button
-                className="btn btn-primary m-1 w-25 btn-block mt-4"
-                type="submit"
-              >
-                <i className="fas fa-arrow-circle-right" />
-              </button>
-            </form>
-          </div>
-        ) : profile.social && Object.keys(profile.social).length ? (
-          <div className="row social">
-            {profile.social.youtube ? (
-              <a
-                className=" p-2"
-                href={"https://" + profile.social.youtube}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-youtube fa-2x" />
-              </a>
-            ) : null}
-            {profile.social.twitter ? (
-              <a
-                className=" p-2"
-                href={"https://" + profile.social.twitter}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-twitter fa-2x" />
-              </a>
-            ) : null}
-            {profile.social.facebook ? (
-              <a
-                className=" p-2"
-                href={"https://" + profile.social.facebook}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-facebook fa-2x" />
-              </a>
-            ) : null}
-            {profile.social.instagram ? (
-              <a
-                className=" p-2"
-                href={"https://" + profile.social.instagram}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-instagram fa-2x" />
-              </a>
-            ) : null}
-            {profile.social.website ? (
-              <a
-                className=" p-2"
-                href={"https://" + profile.social.website}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-globe fa-2x" />
-              </a>
-            ) : null}
-          </div>
-        ) : (
-              <span className="font-italic">
-                {profile.username} is not a social type
-          </span>
-            )}
+        <DescriptionTitle
+          name="social"
+          title="Social"
+          isEditing={editing.social}
+          canEdit={personal}
+          onClick={this.toggleForm}
+        />
+        {editing.social ?
+          <DescriptionForm onSubmit={this.onSubmit("social")}>
+            {this.renderSocialInputFields()}
+          </DescriptionForm>
+          :
+          Object.keys(social).length ?
+            <div className="row social">
+              {this.renderSocialIcons(social)}
+            </div>
+            :
+            <span className="font-italic">
+              {username} is not a social type
+            </span>
+        }
       </React.Fragment>
     );
   }
